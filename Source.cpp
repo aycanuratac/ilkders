0cc977532c3c288e56ad61bed42dc5d4c4caf37a
unknown <aycanuratac4206@gmail.com>
2022-10-28 18:01:49 +0300

    ders sonu

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-	string x;
-	cout << "lutfen yasinizi giriniz: ";
-	cin >> x;
-	cout << "Merhabalar amina kodumun " << x << "'i";
+	string islem = "random";
+	cout << "Isleminizi giriniz(toplama, cikarma, carpma, bolme): ";
+	int ilkSayi, ikinciSayi;
+	cout << "Merhabalar gireceginiz iki sayiyi toplayacagim\n"
+		<< "ilk sayiyi giriniz: ";
+	cin >> ilkSayi;
+	cout << "Ikinci sayiyi giriniz: ";
+	cin >> ikinciSayi;
+	cout << "Isleminizin sonucu: " << (ilkSayi + ikinciSayi) << " dir";
+	cin >> islem;
+	if (islem == "toplama") {
+		// burayi doldur
+	}
+	else if (islem == "cikarma") {
+		// burayi doldur
+	}
+	else {
+		// burayi doldur
+	}
 	return 0;
 }
